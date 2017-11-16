98659e6f8eb92539d015ced28f5359b5b9c283ce
velocityzen <velocityzen@gmail.com>
2017-11-16 14:49:20 -0500

    Fixed simple view class to propper respond on server errors

--- views/simple.js
+++ views/simple.js
@@ -1,17 +1,26 @@
 'use strict';
-const SimpleView = function(template) {
-  this.templateFile = template;
+const Simple = function(template) {
+  this.file = template;
 };
 
-SimpleView.prototype.__init = function(units) {
-  this.env = units.require('core.template');
+Simple.prototype.__init = function(units) {
+  this.logger = units.require('core.logger').get('templates');
+  const env = units.require('templates.nunjucks');
+  this.template = env.getTemplate(this.file, true);
 };
 
-SimpleView.prototype.get = function(req, res) {
-  this.env.render(this.templateFile, req.body, (err, html) => {
+Simple.prototype.get = function(req, res) {
+  this.template.render(req.body, (err, html) => {
+    if (err) {
+      this.logger.error(err);
+      res.status(500).end();
+      return;
+    }
+
+    res.set('Content-Type', 'text/html');
     res.send(html);
   });
 };
 
 
-module.exports = SimpleView;
+module.exports = Simple;
